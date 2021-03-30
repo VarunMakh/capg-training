6830136850ddd6b850449725067cd0f138cad805
VarunMakh <52023918+VarunMakh@users.noreply.github.com>
2021-03-30 16:42:37 +0530

    Add files via upload

--- EmployeeSalary.java
+++ EmployeeSalary.java
@@ -6,29 +6,35 @@ public class EmployeeSalary {
 	
 public static void main(String[] args) {
 		
-		double basicSalary, hra, da, tax=0, totalSalary;
-		System.out.println("Please enter the Basic Salary for the Employee");
+		double basicSalary, hra, da, tax=0, totalSalaryMonthly, totalSalaryAnnual, finalAnnualSalary;
+		System.out.println("Please enter the Basic Salary (Monthly) for the Employee");
 		Scanner sc = new Scanner(System.in);
-		basicSalary = sc.nextInt();
+		basicSalary = sc.nextDouble();
 		sc.close();
 		
 		hra = 0.4 * basicSalary;
 		da = 0.6 * (basicSalary + hra);
+		totalSalaryMonthly = basicSalary + hra + da;
+		totalSalaryAnnual = totalSalaryMonthly * 12;
 		
-		if(basicSalary < 200000) {
+		if(totalSalaryAnnual < 200000) {
 			tax = 0;
 		}
-		else if(basicSalary >= 200000 && basicSalary <=600000) {
-			tax = 0.1 * basicSalary;
+		else if(totalSalaryAnnual >= 200000 && totalSalaryAnnual <=600000) {
+			tax = 0.1 * totalSalaryAnnual;
 		}
-		else if(basicSalary > 600000) {
-			tax = 0.2 * basicSalary;
+		else if(totalSalaryAnnual > 600000) {
+			tax = 0.2 * totalSalaryAnnual;
 		}
+		finalAnnualSalary = totalSalaryAnnual - tax;
 		
-		totalSalary = basicSalary + hra + da - tax;
 		
-		
-		System.out.print("The Annual Take-Home Salary for the employee is: " + totalSalary + "\nThe Annual Tax is: " + tax + "\nThe Annual HRA is: " + hra + "\nThe Annual DA is: " + da + "\n");
+		System.out.print("The Annual Take-Home Salary for the employee is: " + finalAnnualSalary + 
+				"\nThe Annual Salary (Before Tax Reduction) is: " + totalSalaryAnnual +
+				"\nThe Annual Tax is: " + tax + 
+				"\nThe Monthly Salary (Before Tax Reduction) is: " + totalSalaryMonthly +
+				"\nThe HRA is: " + hra +
+				"\nThe DA is: " + da + "\n");
 	}
 
 }
